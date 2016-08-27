164767ca378fe73ac6df26c1791f4c5b1d37cca9
Hina Sakazaki <hinasakazaki@gmail.com>
2016-08-27 10:01:31 -0700

    Yay audio loading from url into game works

--- Assets/scripts/AudioLoader.cs
+++ Assets/scripts/AudioLoader.cs
@@ -5,39 +5,67 @@ using UnityEngine.Events;
 
 public class AudioLoader : MonoBehaviour
 {
-	public AudioSource source;
+	public AudioSource _source;
 	private float audioLength;
-	string URL; //has to be to .ogg
-	// https://en.wikipedia.org/wiki/File:Sample_of_%22Another_Day_in_Paradise%22.ogg
+	private AudioClip _audio;
+	string _url; //has to be to .ogg
+	// https://upload.wikimedia.org/wikipedia/en/9/9f/Sample_of_%22Another_Day_in_Paradise%22.ogg
+
+	private bool finished = false;
 
 	public void onEdited(string url) {
 		Debug.Log("edited "+  url);
-		URL = url;
+		_url = url;
+	}
+
+	public void EnterButtonPressed()
+	{
+		StartCoroutine(StartDownload());
 	}
 	
-	public void StartDownload() 
+	public IEnumerator StartDownload() 
 	{
-		WWW www = new WWW(URL);
-		source = GetComponent<AudioSource>();
-		source.clip = www.GetAudioClip(false, false, AudioType.OGGVORBIS);
-		audioLength = source.clip.length;
+		WWW www = new WWW(_url);
+		
+		yield return www;
+
+		_source = GetComponent<AudioSource>();
+		_audio = www.GetAudioClip(false, false, AudioType.OGGVORBIS);
+		_audio.LoadAudioData();
+		Resources.Load<AudioClip>();
+		_source.clip = _audio;
+		Debug.Log("what is this clip" + _source.clip);
+		Debug.Log("Is source playing?" + _source.isPlaying);
 	}
 
 	void Update() 
 	{
-		if (source.clip != null && !source.isPlaying && source.clip.loadState == AudioDataLoadState.Loaded) 
+
+		if (_source.clip != null && !_source.isPlaying && _source.clip.isReadyToPlay) 
 		{
-			Debug.Log("HINA");
+			Debug.Log("JSDFKKF");
 			onFinished();
+/**			Debug.Log("Going to switch statement!");
+			switch (source.clip.loadState)
+			{
+				case (AudioDataLoadState.Unloaded): Debug.Log("Unloaded"); break;
+				case (AudioDataLoadState.Loading): Debug.Log("Loading"); break;
+				case (AudioDataLoadState.Loaded): onFinished(); finished = true; break;
+				case (AudioDataLoadState.Failed): onFailed(); finished = true; break;
+			}
+			**/
 		} 
 	}
 
+	void onFailed() {
+		Debug.Log("FAILED");
+	}
 	void onFinished() 
 	{
 		Debug.Log("Finished loading sound");
 		gameObject.AddComponent<AudioProcessor>();
 		gameObject.AddComponent<AudioMapper>();
 		gameObject.GetComponent<AudioMapper>().Length = this.audioLength;
-		source.Play();
+		_source.Play();
 	}
 }
